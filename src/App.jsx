cbb9b85d995c967b8221c7bd0235aba49b89e6bc
vladimir.ceballos@gmail.com <vladimir.ceballos@gmail.com>
2024-03-22 12:29:40 -0400

    commit logica del juego canva confetti

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,101 @@
 import { useState } from 'react'
 import './index.css'
+import confetti from 'canvas-confetti'
 
 
 
 
 
 
-const Square = ({children,isSelected,updateBoard,index})=>{
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? "is-selected" : ''}`
-  const handleClick =() =>{
-      updateBoard(index)
-}
-  return(
+  const handleClick = () => {
+    updateBoard(index)
+  }
+  return (
     <div onClick={handleClick} className={className}>
-           {children}     
+      {children}
     </div>
   )
 }
 
+
+const WINNER_COMBOS =[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [0,4,8],
+  
+]
+
 function App() {
 
-  const TURNS ={
+  const TURNS = {
     X: 'X',
     O: '0'
   }
- 
- 
-  const [board, setBoard] = useState(Array(9).fill(null))
+
+
+  const [board, setBoard] = useState(Array(9).fill(null)) // null es que no hay ganador  false es que hay empate 
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
+
+
+  const checkWinner = (boardToCheck)=>{
+    for (const combo of WINNER_COMBOS) {
+     const [a,b,c] = combo
+        if( boardToCheck[a] && // 0 =>  X or O contenga 
+
+          boardToCheck[a] === boardToCheck[b] &&
+          boardToCheck[a] === boardToCheck[c]
+        ){
+          return boardToCheck[a] //0 =>  X or O contenga 
+        }
+
+    }
+    return null
+
+  }
+
+  const resetGame =() =>{
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const checkEndGame = (newBoard) =>{
+
+    return newBoard.every((Square) => Square != null)
+  }
 
   const updateBoard = (index) => {
 
     // no actualizamos esta posición
     // si ya tiene algo
-    if (board[index]) return
-    
-  const newBoard = [...board]
-  newBoard[index]= turn
-  setBoard(newBoard)
-  const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+  
+    if (board[index]  || winner) return
+    // actualizar el tablero 
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    // cambiar el turno 
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
 
     setTurn(newTurn)
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){  // estados asingronos
+      confetti()
+      setWinner(newWinner)
+    } else if(checkEndGame(newBoard)){
+      setWinner(false)// empate
+    }
+
+    
 
   }
 
@@ -48,28 +103,30 @@ function App() {
     <main>
       <h1 className="board">
         The Game Old Make For Vladimir Ceballos
+        <button onClick={resetGame}> Reset del Juego </button>
       </h1>
+    
       <section className="game">
         {
-          board.map((_,index) => {
-            return(
+          board.map((square, index) => {
+            return (
               <Square key={index} updateBoard={updateBoard} index={index}>
-                {board[index]}
-              </Square>  
+                {square}
+              </Square>
 
             )
           })
-         }
+        }
 
       </section>
 
-    
 
-       {TURNS.X==TURNS.X}
-       
-      
-       
-       <section className='turn'>
+
+      {TURNS.X == TURNS.X}
+
+
+
+      <section className='turn'>
         <Square isSelected={turn === TURNS.X}>
           {TURNS.X}
         </Square>
@@ -77,8 +134,31 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+      {
+      winner != null && (
+        <section className="winner">
+          
+          <div className="text">
+            <h2>
+              {
+                winner === false
+                ? 'Empate':'Gano '
+              }
+            </h2>  
+            <header className="win">
+              {winner && <Square>{winner} </Square>}
+            </header>
 
-
+            <footer>
+             <button onClick={resetGame}>
+                 Empezar de nuevo 
+              </button>  
+            </footer>
+          </div>
+          
+        </section>
+      )
+    }
     </main>
   )
 }
